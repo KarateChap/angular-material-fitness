d35eb55b16dbeb4d539e82fb26d8de7c72fde713
gab <gabrielmarianoofficial@gmail.com>
2022-10-01 16:29:01 +0800

    added timer exit functionality

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingComponent implements OnInit {
 
+  ongoingTraining = false;
+
+
   constructor() { }
 
   ngOnInit(): void {
